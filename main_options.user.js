30d9e74bb3070d31307572d252d74606a15ec7df
Max Stapelfeld <nasak.o@hotmail.de>
2016-12-07 18:59:09 +0100

    init settings table

--- main_options.user.js
+++ main_options.user.js
@@ -19,8 +19,23 @@
  */
 
 var $ = typeof unsafeWindow != 'undefined' ? unsafeWindow.$ : window.$;
+var _version = "0.1";
+var _Anleitungslink = "#";
 
 $(function(){
+    var storage = localStorage;
+    var storagePrefix="DS_Box_";
+    //Speicherfunktionen
+    function storageGet(key,defaultValue) {
+        var value= storage.getItem(storagePrefix+key);
+        return (value === undefined || value === null) ? defaultValue : value;
+    }
+    function storageSet(key,val) {
+        storage.setItem(storagePrefix+key,val);
+    }
+    storageSet("manager",storageGet("manager",'{"att":{"runtime":"10","pausetime":"-1","groupid":"0"}}'));
+    storageSet("groups",storageGet("groups",'{"0":"alle"}'));
+
     init_UI();
     function init_UI(){
         //create UI_link
@@ -53,7 +68,7 @@ $(function(){
             "z-index":"100000",
             "left":"50px",
             "top":"50px",
-            "width":"400px",
+            "width":"800px",
             "height":"400px",
             "background-color":"white",
             "border":"1px solid black",
@@ -75,12 +90,23 @@ $(function(){
         }
 
         //Head
-        $("<h3>").text("DS_Box Meta Optionen").appendTo(settingsDiv);
-        $("<span>").text("Legt die Reihenfolge fest, in der die Scripte ausgeführt werden sollen. Für eine genaue Anleitung bitte auf 'Anleitung' klicken").appendTo(settingsDiv);
+        $("<h3>").text("DS_Box Meta Optionen, ").appendTo(settingsDiv).append($("<a>").attr("href","https://raw.githubusercontent.com/st4bel/DS_Box/master/main_options.user.js").text("Version "+_version));
+        $("<span>").text("Legt die Reihenfolge fest, in der die Scripte ausgeführt werden sollen. Für eine genaue Anleitung bitte auf 'Anleitung' klicken.\n").appendTo(settingsDiv);
 
         //Settings
-        var settingsTable=$("<table>").appendTo(settingsDiv);
+        var scriptmanager_table=$("<table>").appendTo(settingsDiv);
+        //script/runtime/pausetime/groupid/delete
+        //header:
+        $("<tr>").append($("<th>").text("script_id")).append($("<th>").text("max. runtime in min")).append($("<th>").text("pausetime")).append($("<th>").text("groupid")).append($("<th>").text("delete"))
+        .appendTo(scriptmanager_table);
+        for(var id in JSON.parse(storageGet("manager"))){
+            addRowToScriptmanager(id);
+        }
 
+        $("<button>").text("Gruppen aktualisieren").click(function(){
+            readGroups();
+        }).appendTo(settingsDiv);
+        $("<br>").appendTo(settingsDiv);
         //Foot
         $("<button>").text("Schließen").click(function(){
             toggleSettingsVisibility();
@@ -88,5 +114,80 @@ $(function(){
         $("<button>").text("Anleitung").click(function(){
             window.open(_Anleitungslink, '_blank');
         }).appendTo(settingsDiv);
+        //UI - functions
+        function addRowToScriptmanager(id){
+            var manager = JSON.parse(storageGet("manager"))[id];
+            var script_cell = $("<td>").text("ID: "+id);
+            var runtime_cell = $("<td>")
+            .append(
+                $("<input>")
+                .attr("type","text")
+                .attr("name",id)
+                .val(manager.runtime)
+                .on("input",function(){
+                    var m =  JSON.parse(storageGet("manager"));
+                    m[$(this).attr("name")].runtime = $(this).val();
+                    storageSet("manager",JSON.stringify(m));
+                    console.log("manager changed to: "+storageGet("manager"));
+                })
+            );
+            var pausetime_cell = $("<td>")
+            .append(
+                $("<input>")
+                .attr("type","text")
+                .attr("name",id)
+                .val(manager.pausetime)
+                .on("input",function(){
+                    var m =  JSON.parse(storageGet("manager"));
+                    m[$(this).attr("name")].pausetime = $(this).val();
+                    storageSet("manager",JSON.stringify(m));
+                    console.log("manager changed to: "+storageGet("manager"));
+                })
+            );
+            var groupselect_cell = $("<td>")
+            .append(
+                $("<select>")
+                .attr("name",id)
+                .change(function(){
+                    var m =  JSON.parse(storageGet("manager"));
+                    m[$(this).attr("name")].groupid = $("option:selected",$(this)).val();
+                    storageSet("manager",JSON.stringify(m));
+                    console.log("manager changed to: "+storageGet("manager"));
+                })
+            );
+            var groups = JSON.parse(storageGet("groups"));
+            for(var gid in groups){
+                $("<option>")
+                .text(groups[gid])
+                .attr("value",gid)
+                .appendTo($("select",groupselect_cell));
+            }
+            $("option[value="+manager.groupid+"]",groupselect_cell).prop("selected",true);
+            var other_cell = $("<td>");
+
+            //appending everything
+            $("<tr>").attr("name",id)
+            .append(script_cell).append(runtime_cell).append(pausetime_cell).append(groupselect_cell).append(other_cell)
+            .appendTo(scriptmanager_table);
+        }
+    }
+    function readGroups(){
+        var groups = {};
+        $("a.group-menu-item").each(function(){
+            groups[getGroupIdOutOfElement($(this))] = $(this).text().substring(1,$(this).text().length-1);
+        });
+        //current group:
+        $("strong.group-menu-item").each(function(){
+            groups[getGroupIdOutOfElement($("#village_switch_right"))] = $(this).text().substring(1,$(this).text().length-2);
+        });
+        function getGroupIdOutOfElement(handle){
+            var link = handle.attr("href");
+            var pos1 = link.indexOf("group=")+6;
+            var pos2 = link.indexOf("&",pos1)!=-1?link.indexOf("&",pos1) : link.length;
+            return link.substring(pos1,pos2);
+        }
+
+        storageSet("groups",JSON.stringify(groups));
+        console.log("Set groups to: "+storageGet("groups"));
     }
 });
